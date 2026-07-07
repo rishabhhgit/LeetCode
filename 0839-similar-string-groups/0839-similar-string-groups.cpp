ed1c7e27ef54c0c5a4c56f081393165c8abb463f
rishabh <rishabhh.steam@gmail.com>
2026-07-07 19:00:49 +0530

    Time: 897 ms (5.34%), Space: 483.2 MB (5.04%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    vector<int>parent;
+    vector<int>rank;
+    bool check(string x,string y){
+        
+        if(x.size()!=y.size()) return false;
+        vector<int>diff;
+        for(int i=0;i<x.size();i++){
+            if(x[i]!=y[i]){
+                diff.push_back(i);
+            }
+        }
+        if(diff.size()==0) return true;
+        else if(diff.size()!=2) return false;
+
+        return x[diff[0]]==y[diff[1]] && x[diff[1]]==y[diff[0]];
+    }
+
+    int find(int i){
+        if(i==parent[i]) return i;
+        return parent[i]=find(parent[i]);
+    }
+
+    void uni(int x,int y){
+        int parent_x=find(x);
+        int parent_y=find(y);
+
+        if(parent_x==parent_y) return;
+
+        if(rank[parent_y]>rank[parent_x]){
+            parent[parent_x]=parent_y;
+        }
+        else if(rank[parent_y]<rank[parent_x]){
+            parent[parent_y]=parent_x;
+        }
+        else{
+            parent[parent_y]=parent_x;
+            rank[parent_x]++;
+        }
+    }
+
+    int numSimilarGroups(vector<string>& s) {
+        parent.resize(s.size());
+        for(int i=0;i<s.size();i++){
+            parent[i]=i;
+        }
+        rank.resize(s.size(),0);
+
+        for(int i=0;i<s.size();i++){
+            for(int j=i+1;j<s.size();j++){
+                if(check(s[i],s[j])){
+                    uni(i,j);
+                }
+            }
+        }
+        int count=0;
+        unordered_set<int>s1;
+        for(int i=0;i<s.size();i++){
+            s1.insert(find(i));
+        }
+        return s1.size();
+    }
+};
