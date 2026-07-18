2ba4dcb0d688e13e28319d0bae54d7fd0e665a62
rishabh <rishabhh.steam@gmail.com>
2026-07-18 17:56:54 +0530

    Time: 1764 ms (6.67%), Space: 147.1 MB (6.6%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,28 +1,29 @@
+
 class Solution {
 public:
+    map<pair<int,int>,int>arr;
+    int solve(int i, vector<int>& range, int n, int maxEnd) {
+        if (i >= range.size()) {
+            if(maxEnd>=n) return 0;
+            else return 1e9;
+        }
+
+        if (i > maxEnd) return 1e9;
+        if(arr.find({i,maxEnd})!=arr.end()) return arr[{i,maxEnd}];
+        int skip = solve(i + 1, range, n, maxEnd);
+        int take = solve(i + 1, range, n, max(maxEnd, range[i])) + 1;
+        return arr[{i,maxEnd}]=min(skip, take);
+    }
     int minTaps(int n, vector<int>& nums) {
-        //FIND BEST RANGE FOR EACH INDEX
         vector<int>range(n+1,0);
-        for(int i=0;i<n+1;i++){
+        for(int i=0;i<nums.size();i++){
             int lowerRange=max(0,i-nums[i]);
             int upperRange=min(n,i+nums[i]);
 
             range[lowerRange]=max(range[lowerRange],upperRange);
         }
-        int bestMaxEnd=0;
-        int totalTaps=0;
-        int currMaxEnd=0;
-        int i=0;
-        while(i<n){
-            bestMaxEnd=max(bestMaxEnd,range[i]);
-            if(i==currMaxEnd){
-                if(currMaxEnd==bestMaxEnd) return -1;
-                totalTaps++;
-                currMaxEnd=bestMaxEnd;
-            }
-            
-            i++;
-        }
-        return totalTaps;
+        int ans=solve(0, range, n, 0);
+        if(ans==1e9) return -1;
+        return ans;
     }
 };
