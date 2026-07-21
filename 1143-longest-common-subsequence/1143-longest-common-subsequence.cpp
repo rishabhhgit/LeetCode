da3ce2708dd0db2714cb47e9d65175c6bb088990
rishabh <rishabhh.steam@gmail.com>
2026-07-21 19:15:21 +0530

    Time: 31 ms (38.3%), Space: 12.9 MB (86.67%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arr[1001][1001];
+    int check(string &s1, string &s2,int i,int j){
+        if(i>=s1.size() || j>=s2.size()) return 0;
+        if(arr[i][j]!=-1) return arr[i][j];
+        if(s1[i]==s2[j]) return arr[i][j]=1+check(s1,s2,i+1,j+1);
+        return arr[i][j]=max(check(s1,s2,i+1,j),check(s1,s2,i,j+1));
+    }
+    int longestCommonSubsequence(string s1, string s2) {
+        memset(arr,-1,sizeof(arr));
+        return check(s1,s2,0,0);
+    }
+};
