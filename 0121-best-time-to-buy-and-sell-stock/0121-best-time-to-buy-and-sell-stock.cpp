605534949def672e9ac7879da5078d2032cc4d95
rishabh <rishabhh.steam@gmail.com>
2026-08-03 04:23:53 +0530

    Time: 4 ms (17.8%), Space: 97.2 MB (88.14%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
-    int arr[100001][2][2];
-    int solve(int i,vector<int>& nums,pair<bool,bool>check){
-        if(i>=nums.size()) return 0;
-        
-        int buy=0;
-        int sell=0;
-        bool canBuy=check.first;
-        bool sellAvailable=check.second;
-
-        if(arr[i][canBuy][sellAvailable]!=-1) return arr[i][canBuy][sellAvailable];
-
-        int skip=solve(i+1,nums,{canBuy,sellAvailable});
-        if(canBuy){
-            buy=solve(i+1,nums,{false,true})-nums[i];
-        }
-        if(sellAvailable){
-            sell=solve(i+1,nums,{false,false})+nums[i];
-        }
-        return arr[i][canBuy][sellAvailable]=max(skip,max(buy,sell));
-    }
     int maxProfit(vector<int>& nums) {
-        memset(arr,-1,sizeof(arr));
-        return solve(0,nums,{true,false});
+        long long minPrice=INT_MAX;
+        long long maxProfit=0;
+
+        for(int i=0;i<nums.size();i++){
+            minPrice=min(minPrice,(long long)nums[i]);
+            maxProfit=max(maxProfit,nums[i]-minPrice);
+        }
+        return maxProfit;
     }
 };
