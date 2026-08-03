75d09e3d9b898a91230b8c9b9161b53d8759faad
rishabh <rishabhh.steam@gmail.com>
2026-08-03 23:29:46 +0530

    Time: 4 ms (21.02%), Space: 40.3 MB (61.63%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,43 +1,19 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int>rightProduct(nums.size());
-        vector<int>leftProduct(nums.size());
+        vector<int>result(nums.size(),1);
+        int n=nums.size();
 
-        //calculate right Product
-        stack<int>rightStack;
-        for(int i=nums.size()-1;i>=0;i--){
-            if(rightStack.empty()){
-                rightProduct[i]=1;
-                rightStack.push(nums[i]);
-            }
-            else{
-                int topValue=rightStack.top();
-                rightStack.pop();
-                rightProduct[i]=topValue;
-                rightStack.push(nums[i]*topValue);
-            }
+        int leftVal=1;
+        for(int i=0;i<n;i++){
+            result[i]*=leftVal;
+            leftVal*=nums[i];
         }
-
-        //calculate right Product
-        stack<int>leftStack;
-        for(int i=0;i<=nums.size()-1;i++){
-            if(leftStack.empty()){
-                leftProduct[i]=1;
-                leftStack.push(nums[i]);
-            }
-            else{
-                int topValue=leftStack.top();
-                leftStack.pop();
-                leftProduct[i]=topValue;
-                leftStack.push(nums[i]*topValue);
-            }
+        int rightVal=1;
+        for(int i=n-1;i>=0;i--){
+            result[i]*=rightVal;
+            rightVal*=nums[i];
         }
-        vector<int>ans(nums.size());
-        //traverse through the array
-        for(int i=0;i<nums.size();i++){
-            ans[i]=rightProduct[i]*leftProduct[i];
-        }
-        return ans;
+        return result;
     }
 };
