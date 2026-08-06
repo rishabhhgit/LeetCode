b240d920f4eff1e828830b594c5f02a831a5596e
rishabh <rishabhh.steam@gmail.com>
2026-08-06 15:51:29 +0530

    Time: 7 ms (84.74%), Space: 9.3 MB (77.61%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    int arr[1001][1001];
-    bool check(string &s,int i,int j){
-        if(i>=j) return true;
-        if(arr[i][j]!=-1) return arr[i][j];
-        if(s[i]==s[j]) return arr[i][j]=check(s,i+1,j-1);
-        else return arr[i][j] = false;
-    }
-
-    string solve(string &s){
-        int maxLength=0;
-        int startIdx=-1;
-
-        for(int i=0;i<s.size();i++){
-            for(int j=i;j<s.size();j++){
-                if(check(s,i,j)){
-                    if(j-i+1>maxLength){
-                        maxLength=j-i+1;
-                        startIdx=i;
-                    }
-                }
-            }
-        }
-        return s.substr(startIdx,maxLength);
-
-    }
     string longestPalindrome(string s) {
-        memset(arr,-1,sizeof(arr));
-        return solve(s);
+        if (s.empty()) return "";
+        int start = 0, maxLen = 0;
+        
+        auto expand = [&](int left, int right) {
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            int len = right - left - 1;
+            if (len > maxLen) {
+                maxLen = len;
+                start = left + 1;
+            }
+        };
+
+        for (int i = 0; i < s.size(); i++) {
+            expand(i, i);
+            expand(i, i + 1);
+        }
+        
+        return s.substr(start, maxLen);
     }
 };
