7eb51edb7a55738f978ad61a6df41fcd3f0377e8
rishabh <rishabhh.steam@gmail.com>
2026-08-07 23:33:24 +0530

    Time: 4 ms (10.54%), Space: 71.7 MB (53.61%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,8 +5,8 @@ public:
         int sum=0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
+            maxi=max(maxi,sum);
             if(sum<0) sum=0;
-            if(sum!=0) maxi=max(maxi,sum);
         }
         return maxi;
     }
