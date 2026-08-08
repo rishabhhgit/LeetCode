655a19c664a27644da5fbf20b39ca9c85b63282b
rishabh <rishabhh.steam@gmail.com>
2026-08-09 02:54:49 +0530

    Time: 7 ms (84.67%), Space: 9.5 MB (48.98%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,31 @@
+#include <string>
+#include <vector>
+
 class Solution {
 public:
     string longestPalindrome(string s) {
         if (s.empty()) return "";
         int start = 0, maxLen = 0;
         
-        auto expand = [&](int left, int right) {
-            while (left >= 0 && right < s.size() && s[left] == s[right]) {
-                left--;
-                right++;
-            }
-            int len = right - left - 1;
+        for (int i = 0; i < s.length(); ++i) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = std::max(len1, len2);
+            
             if (len > maxLen) {
                 maxLen = len;
-                start = left + 1;
+                start = i - (len - 1) / 2;
             }
-        };
-
-        for (int i = 0; i < s.size(); i++) {
-            expand(i, i);
-            expand(i, i + 1);
         }
-        
         return s.substr(start, maxLen);
+    }
+
+private:
+    int expandAroundCenter(const string& s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
     }
 };
