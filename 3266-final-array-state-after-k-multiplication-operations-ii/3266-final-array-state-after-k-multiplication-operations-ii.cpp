efe7a087e7f2d5c299096a52e99a9b942e25376a
rishabh <rishabhh.steam@gmail.com>
2026-08-11 22:48:01 +0530

    Time: 76 ms (58.39%), Space: 63.9 MB (27.27%) - LeetHub

--- 3266-final-array-state-after-k-multiplication-operations-ii/3266-final-array-state-after-k-multiplication-operations-ii.cpp
+++ 3266-final-array-state-after-k-multiplication-operations-ii/3266-final-array-state-after-k-multiplication-operations-ii.cpp
@@ -1,18 +1,14 @@
-#include <vector>
-#include <queue>
-#include <algorithm>
 
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
-    long long MOD = 1e9 + 7;
-
-    long long power(long long base, long long exp) {
+    long long power(long long base, long long exp, long long mod) {
         long long res = 1;
-        base %= MOD;
+        base %= mod;
         while (exp > 0) {
-            if (exp % 2 == 1) res = (res * base) % MOD;
-            base = (base * base) % MOD;
+            if (exp % 2 == 1) res = (__int128)res * base % mod;
+            base = (__int128)base * base % mod;
             exp /= 2;
         }
         return res;
@@ -21,7 +17,9 @@ class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         if (multiplier == 1) return nums;
+
         int n = nums.size();
+        long long mod = 1e9 + 7;
         priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
 
         long long max_val = 0;
@@ -30,32 +28,40 @@ public:
             max_val = max(max_val, (long long)nums[i]);
         }
 
-        while (k > 0) {
-            auto [val, idx] = pq.top();
+        // Perform operations until all elements are roughly equal or k is exhausted
+        while (k > 0 && pq.top().first * multiplier <= max_val) {
+            pair<long long, int> top = pq.top();
             pq.pop();
-            if (val * multiplier > max_val) {
-                pq.push({val, idx});
-                break;
-            }
-            val *= multiplier;
-            nums[idx] = val;
-            max_val = max(max_val, (long long)val);
-            pq.push({val, idx});
+            top.first *= multiplier;
+            pq.push(top);
             k--;
         }
 
+        // Now all elements are close, distribute remaining k operations
         int full_rounds = k / n;
         int remaining = k % n;
 
-        for (int i = 0; i < n; ++i) {
-            long long val = pq.top().first;
-            int idx = pq.top().second;
+        long long mult_pow = power(multiplier, full_rounds, mod);
+
+        vector<pair<long long, int>> final_pq;
+        while (!pq.empty()) {
+            final_pq.push_back(pq.top());
             pq.pop();
-            
-            long long multiplier_pow = power(multiplier, full_rounds + (i < remaining ? 1 : 0));
-            nums[idx] = (val % MOD * multiplier_pow) % MOD;
         }
 
-        return nums;
+        // Sort by value then index to correctly distribute the 'remaining' operations
+        sort(final_pq.begin(), final_pq.end());
+
+        vector<int> result(n);
+        for (int i = 0; i < n; ++i) {
+            long long val = final_pq[i].first % mod;
+            val = (__int128)val * mult_pow % mod;
+            if (i < remaining) {
+                val = (__int128)val * multiplier % mod;
+            }
+            result[final_pq[i].second] = (int)val;
+        }
+
+        return result;
     }
 };
