72b91c80b55a6ca8a74d147aa655700695398464
rishabh <rishabhh.steam@gmail.com>
2026-08-11 18:54:39 -0700

    Time: 0 ms (100%), Space: 54.9 MB (86.27%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -2,30 +2,16 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-
-        // First pass: mark numbers that are out of range
-        for (int i = 0; i < n; i++) {
-            if (nums[i] <= 0 || nums[i] > n) {
-                nums[i] = n + 1;
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[nums[i] - 1], nums[i]);
             }
         }
-
-        // Second pass: use index as a hash key and mark the presence of numbers
-        for (int i = 0; i < n; i++) {
-            int num = abs(nums[i]);
-            if (num <= n) {
-                nums[num - 1] = -abs(nums[num - 1]);
-            }
-        }
-
-        // Third pass: find the first cell which isn't negative
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) {
                 return i + 1;
             }
         }
-
-        // If all numbers from 1 to n are present, return n + 1
         return n + 1;
     }
 };
