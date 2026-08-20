db3639b228bb31f726b970176f3614fbf1e94cfa
rishabh <rishabhh.steam@gmail.com>
2026-08-21 00:00:27 +0530

    Time: 28 ms (98.29%), Space: 19.3 MB (96.65%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,37 +1,46 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> st(wordList.begin(), wordList.end());
-        if (st.find(endWord) == st.end()) return 0;
-
-        queue<string> q;
-        q.push(beginWord);
-        int steps = 1;
-
-        while (!q.empty()) {
-            int sz = q.size();
-            for (int i = 0; i < sz; ++i) {
-                string curr = q.front();
-                q.pop();
-
-                if (curr == endWord) return steps;
-
-                for (int j = 0; j < curr.size(); ++j) {
-                    char orig = curr[j];
-                    for (char c = 'a'; c <= 'z'; ++c) {
-                        if (c == orig) continue;
-                        curr[j] = c;
-                        if (st.count(curr)) {
-                            st.erase(curr);
-                            q.push(curr);
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        if (dict.find(endWord) == dict.end())
+            return 0;
+        dict.erase(beginWord);
+        unordered_set<string> head, tail, *phead, *ptail;
+        head.insert(beginWord);
+        tail.insert(endWord);
+        int dist = 2;
+        while (!head.empty() && !tail.empty()) {
+            if (head.size() <= tail.size()) {
+                phead = &head;
+                ptail = &tail;
+            } else {
+                phead = &tail;
+                ptail = &head;
+            }
+            unordered_set<string> temp;
+            for (auto it = phead->begin(); it != phead->end(); ++it) {
+                string word = *it;
+                for (int i = 0; i < word.length(); i++) {
+                    char orig = word[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == orig)
+                            continue;
+                        word[i] = c;
+                        if (ptail->find(word) != ptail->end()) {
+                            return dist;
+                        }
+                        if (dict.find(word) != dict.end()) {
+                            temp.insert(word);
+                            dict.erase(word);
                         }
                     }
-                    curr[j] = orig;
+                    word[i] = orig;
                 }
             }
-            steps++;
+            dist++;
+            *phead = std::move(temp);
         }
-
         return 0;
     }
 };
