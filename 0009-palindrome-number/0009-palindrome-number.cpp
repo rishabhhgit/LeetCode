e1f416b5963f93031140bde49c154acfb6bb4988
rishabh <rishabhh.steam@gmail.com>
2026-08-21 17:16:28 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.37%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,20 +2,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // Negative numbers are not palindromes (e.g., -121 != 121-)
-        // Also, if the last digit is 0, the first must be 0 (only possible for 0 itself)
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
 
-        int revertedNumber = 0;
-        while (x > revertedNumber) {
-            revertedNumber = revertedNumber * 10 + x % 10;
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
             x /= 10;
         }
 
-        // When the length is odd, we can get rid of the middle digit by revertedNumber/10
-        // For example, in 121, at the end of the loop x = 1, revertedNumber = 12
-        return x == revertedNumber || x == revertedNumber / 10;
+        return x == reversed || x == reversed / 10;
     }
 };
