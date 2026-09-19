945f85b405a9e66b4f189978676c6e84b05e3eb6
rishabh <rishabhh.steam@gmail.com>
2026-09-19 20:32:46 +0530

    Time: 0 ms (100%), Space: 65.1 MB (65.97%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int>indegree(n+1,0);
-        vector<int>outdegree(n+1,0);
+        vector<int>trustKartaHai(n+1,0);
+        vector<int>trustNahiKarta(n+1,0);
 
         for(auto &it:trust){
             int i=it[0];
             int j=it[1];
 
-            indegree[j]++;
-            outdegree[i]++;
+            trustKartaHai[j]++;
+            trustNahiKarta[i]++;
         }
         for(int i=1;i<=n;i++){
-            if(indegree[i]==n-1 && outdegree[i]==0) return i;
+            if(trustKartaHai[i]==n-1 && trustNahiKarta[i]==0) return i;
         }
         return -1;
     }
