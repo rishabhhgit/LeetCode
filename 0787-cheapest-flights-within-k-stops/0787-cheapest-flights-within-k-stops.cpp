9fe41414c2ffe3e62b98e87312d4e35e39400182
rishabh <rishabhh.steam@gmail.com>
2026-09-23 23:20:05 +0530

    Time: 7 ms (29.97%), Space: 18.3 MB (41.45%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        // {distance ,{from,{to,k}}
+        typedef pair<int,pair<int,int>>p;
+        priority_queue<p,vector<p>,greater<p>>pq;
+        unordered_map<int,vector<pair<int,int>> >adj;
+        for(auto &v:flights){
+            int source=v[0];
+            int destination=v[1];
+            int weight=v[2];
+            adj[source].push_back({destination,weight});
+        }
+
+        pq.push({0,{src,0}});
+        vector<vector<int>>result(n,vector<int>(k+2,1e9));
+        result[src][0]=0;
+
+        while(!pq.empty()){
+            int val=pq.top().first;
+            int node=pq.top().second.first;
+            int k_took=pq.top().second.second;
+
+            pq.pop();
+
+            if (node == dst) return val;
+
+            if (k_took == k + 1) continue;
+
+            for(auto &it:adj[node]){
+                int new_node=it.first;
+                int wt=it.second;
+
+                if(wt+val<result[new_node][k_took+1]){
+                    result[new_node][k_took+1]=wt+val;
+                    pq.push({wt+val,{new_node,k_took+1}});
+                }
+            }
+
+        }
+        return -1;
+    }
+};
